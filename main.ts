b52d2731e9e522661e3f97dfe3eacd2e031d9cf6
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-10-04 10:47:22 -0400

    Rgb color

--- main.ts
+++ main.ts
@@ -1,8 +1,36 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Yousef
+ * Created on: OCT 2024
+ * This program RGB color changer
 */
 
-basic.showString('Hello, World!')
+input.onButtonPressed(Button.A, function () {
+    //red
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    basic.pause(1000)
+    //green
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    basic.pause(1000)
+
+    //blue
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+
+})
+
+
+
+
+
+
